b712f80aa7d3c1d4cfd4bd319df2941f4cb757a9
Claudio Cenghialta <claudio.cenghialta@gmail.com>
2020-07-15 15:33:08 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*
+Palidroma
+Chiedere all'utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+var tuaParola = prompt('inserisci una parola');
+while (tuaParola==""||isNaN(tuaParola)==false||tuaParola.length<2){
+    if (tuaParola=="") {
+    tuaParola = prompt('La stringa non può essere vuota. Inserisci una parola');
+    } else if (isNaN(tuaParola)==false){
+    tuaParola = prompt('Non puoi inserire numeri in cifre. Inserisci una parola');
+    } else /*(tuaParola.length<2)*/ {
+    tuaParola = prompt('La parola deve essere almeno di 2 caratteri. Inserisci una parola');
+    }
+}
+var palindromo = checkPalindromo (tuaParola);
+console.log(tuaParola,palindromo);
+
+
+// Definisco la funzione ---------------------------------------------
+function checkPalindromo (parola) {
+    var inverso =""
+    for (i=parola.length-1;i>=0;i--) {
+        inverso += parola[i];
+    }
+    if (parola.toLowerCase()==inverso.toLowerCase()) {
+        return 'E\' palindroma'
+    } else {
+        return 'Non è palindroma'
+    }
+}
+
